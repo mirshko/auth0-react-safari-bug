f9a7c3db91520e4964b7ee6c3c6823d8f807e37e
Jeff Reiner <theundeadghost@gmail.com>
2021-11-09 15:08:23 +0100

    Use env

--- auth0.ts
+++ auth0.ts
@@ -1,4 +1,4 @@
-export const AUTH0_CLIENT_ID = "yKeh5XfaLHv1qaiYyopr7HRHXqGOwkE4";
-export const AUTH0_DOMAIN = "dialectic-debug.eu.auth0.com";
-export const AUTH0_REDIRECT_URI = "http://localhost:3000";
-export const AUTH0_AUDIENCE = "http://localhost:3000/api/hello";
+export const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
+export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
+export const AUTH0_REDIRECT_URI = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI;
+export const AUTH0_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE;
