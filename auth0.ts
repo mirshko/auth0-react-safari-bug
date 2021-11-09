7bca9e9da18a673ea7efbffa55a1d9d7a32f68d0
Jeff Reiner <theundeadghost@gmail.com>
2021-11-09 15:09:40 +0100

    Env

--- auth0.ts
+++ auth0.ts
@@ -1,4 +1,6 @@
-export const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
-export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
-export const AUTH0_REDIRECT_URI = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI;
-export const AUTH0_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE;
+export const AUTH0_CLIENT_ID = process.env
+  .NEXT_PUBLIC_AUTH0_CLIENT_ID as string;
+export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string;
+export const AUTH0_REDIRECT_URI = process.env
+  .NEXT_PUBLIC_AUTH0_REDIRECT_URI as string;
+export const AUTH0_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE as string;
